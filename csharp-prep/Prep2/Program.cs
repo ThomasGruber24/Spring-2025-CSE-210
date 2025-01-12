21eada70398d05a3c8017a73a0efeeec8eed2a8c
Thomas-Gruber24 <thomaspgruber@yahoo.com>
2025-01-11 17:39:34 -0700

    1/11 assignments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("What is your grade? ");
+        string stGrade = Console.ReadLine();
+        int grade = int.Parse(stGrade);
+
+        if (grade > 90)
+        {
+            Console.WriteLine("A, you passed your class!");
+        }
+        else if (grade > 80)
+        {
+            Console.WriteLine("B, you passed your class!");
+        }
+        else if (grade > 70)
+        {
+            Console.WriteLine("C, you passed your class!");
+        }
+        else if (grade > 60)
+        {
+            Console.WriteLine("D, you did not pass this class.");
+        }
+        else
+        {
+            Console.WriteLine("F, you failed this class.");
+        }
     }
 }
