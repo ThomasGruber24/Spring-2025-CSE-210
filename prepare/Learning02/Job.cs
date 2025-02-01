95b432dcab4d54d71308a6a95402893f2360b8c8
ThomasGruber24 <thomaspgruber@yahoo.com>
2025-02-01 13:38:50 -0700

    Fixing some code

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -9,6 +9,6 @@ public class Jobs
 
     public void Display()
     {
-        Console.WriteLine($"{_jobTitle} ({comapny}) {_startYear}-{_endYear}");
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
     }
 }
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -6,20 +6,20 @@ class Program
     {
         Console.WriteLine("Hello Learning02 World!");
 
-        Job job1 = new Job();
+        Jobs job1 = new Jobs();
         job1._jobTitle = "Software Engineer";
         job1._company = "Microsoft";
         job1._startYear = 2019;
         job1._endYear = 2022;
 
-        Job job2 = new Job;
+        Jobs job2 = new Jobs();
         job2._jobTitle = "Manager";
         job2._company = "Apple";
         job2._startYear = 2022;
         job2._endYear = 2023;
 
 
-        Resume myResume = mew Resume();
+        Resume myResume = new Resume();
         myResume._name = "Allison Rose";
 
         myResume._jobs.Add(job1);
