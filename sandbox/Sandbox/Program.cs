b59cc0f44784a190248e2a2c7fbb6de7921d7a46
ThomasGruber24 <thomaspgruber@yahoo.com>
2025-05-06 12:22:12 -0600

    inclass work

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,37 @@
 using System;
 
+class Circle
+{
+    private double _radius;
+
+    public void SetRadius(double radius)
+    {
+        if (radius < 0)
+        {
+            Console.WriteLine("Error, Radius must be > 0");
+            return;
+        }
+        _radius = radius;
+    }
+
+    public double GetRadius()
+    {
+        return _radius;
+    }
+
+    public double GetArea()
+    {
+        return Math.PI * _radius * _radius;
+    }
+}
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // Console.WriteLine("Hello Sandbox World!");
+
+        Circle myCircle = new Circle();
+        myCircle.SetRadius(10);
+        Console.WriteLine($"{myCircle.GetRadius()}");
     }
 }
